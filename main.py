5e859a5370afc7ac368f4a5310e273d0e2c090e0
Jiaming Li <ljm625@gmail.com>
2017-05-18 14:53:53 -0700

    fix issue caused by auto reformat code

--- main.py
+++ main.py
@@ -11,32 +11,31 @@ import logging
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
+    PacGenerator("config.yaml").build_pac()
+    helper = Helper(1, "Thread-1")
+    helper.start()
 
-        PacGenerator("config.yaml").build_pac()
-        helper=Helper(1,"Thread-1")
-        helper.start()
+    server = ThreadedTCPServer(ThreadedTCPRequestHandler)
+    ip, port = server.server_address
 
-        server = ThreadedTCPServer(ThreadedTCPRequestHandler)
-        ip, port = server.server_address
-
-        # Start a thread with the server -- that thread will then start one
-        # more thread for each request
-        server_thread = threading.Thread(target=server.serve_forever)
-        # Exit the server thread when the main thread terminates
-        server_thread.daemon = True
+    # Start a thread with the server -- that thread will then start one
+    # more thread for each request
+    server_thread = threading.Thread(target=server.serve_forever)
+    # Exit the server thread when the main thread terminates
+    server_thread.daemon = True
     logging.info("Sleep a little bit for the proxy to complete..")
-        sleep(10)
-        server_thread.start()
+    sleep(10)
+    server_thread.start()
     logging.info("Server Running at {}".format(port))
-        try:
-            while True:
-                sleep(1)
-        except KeyboardInterrupt:
-            logging.info("User shutdown request received. Starting to shutdown")
-        except Exception as e:
-            logging.error("Critical Unknown issue received. Shutting down. {}".format(e))
-        helper.stop()
+    try:
+        while True:
+            sleep(1)
+    except KeyboardInterrupt:
+        logging.info("User shutdown request received. Starting to shutdown")
+    except Exception as e:
+        logging.error("Critical Unknown issue received. Shutting down. {}".format(e))
+    helper.stop()
     logging.info("Helper Stopped")
-        server.shutdown()
+    server.shutdown()
     logging.info("All Shutdown")
     sys.exit(1)

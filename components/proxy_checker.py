5afcad1222b76d1b3d5aa3482e9a9023fe33747a
Jiaming Li <ljm625@gmail.com>
2018-07-10 14:24:34 +0800

    Added better speed test mechanizm

--- components/proxy_checker.py
+++ components/proxy_checker.py
@@ -6,12 +6,13 @@ import yaml
 class ProxyChecker(object):
     best_proxy=None
     instance=None
-    def __init__(self,proxy_list,check_url,speedtest_url,timeout=10,checker_timeout=20):
+    def __init__(self,proxy_list,check_url,speedtest_url,timeout=10,checker_timeout=20,speedtest_times=1):
         self.proxy_list=proxy_list
         self.check_url=check_url
         self.timeout=timeout
         self.checker_timeout=checker_timeout
         self.speedtest_url=speedtest_url
+        self.speedtest_times = speedtest_times
 
     @classmethod
     def get_instance(cls,proxy_list=None):
@@ -29,8 +30,9 @@ class ProxyChecker(object):
             speedtest_url=config.get('speedtest_url')
             timeout=config.get('timeout')
             checker_timeout=config.get('checker_timeout')
+            speedtest_times=config.get("speedtest_times")
             if check_url and timeout:
-                cls.instance=cls(proxy_list=proxy_list,check_url=check_url,speedtest_url=speedtest_url,timeout=timeout,checker_timeout=checker_timeout)
+                cls.instance=cls(proxy_list=proxy_list,check_url=check_url,speedtest_url=speedtest_url,timeout=timeout,checker_timeout=checker_timeout,speedtest_times=speedtest_times)
                 return cls.instance
             else:
                 raise Exception("ERROR : The Config URL is missing")
@@ -44,11 +46,14 @@ class ProxyChecker(object):
             else:
                 resp=requests.get(self.check_url,proxies=build_proxy(),timeout=self.timeout)
             if resp.status_code < 300 and resp.text=='true':
-                resp_speed = requests.get(self.speedtest_url,proxies=build_proxy(),timeout=self.timeout)
-                if resp_speed.status_code> 300:
-                    raise Exception("Error:Server Speed too slow.")
-                # TODO : The netease validate link always return false whether it's mainland or not
-                return resp_speed.elapsed.total_seconds()*1000
+                total_time =0
+                for i in range(0,self.speedtest_times):
+                    resp_speed = requests.get(self.speedtest_url,proxies=build_proxy(),timeout=self.timeout)
+                    if resp_speed.status_code> 300:
+                        raise Exception("Error:Server Speed too slow.")
+                    # TODO : The netease validate link always return false whether it's mainland or not
+                    total_time += resp_speed.elapsed.total_seconds()*1000
+                return total_time
             return None
         except Exception as e:
             return None
